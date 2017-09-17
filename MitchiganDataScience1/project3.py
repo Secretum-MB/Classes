d10a16483e29d8870a62961aba57acf6a1c60bb4
Rider One <mathiasbube2@gmail.com>
2017-09-16 18:39:06 -0700

    started project 3

---
+++ MitchiganDataScience1/project3.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+"""
+Created on Sat Sep 16 17:27:48 2017
+
+"""
+
+import numpy as np
+import pandas as pd
+
+
+# Use this dictionary to map state names to two letter acronyms
+states = {'OH': 'Ohio', 'KY': 'Kentucky', 'AS': 'American Samoa', 'NV': 'Nevada', 'WY': 'Wyoming', 'NA': 'National', 'AL': 'Alabama', 'MD': 'Maryland', 'AK': 'Alaska', 'UT': 'Utah', 'OR': 'Oregon', 'MT': 'Montana', 'IL': 'Illinois', 'TN': 'Tennessee', 'DC': 'District of Columbia', 'VT': 'Vermont', 'ID': 'Idaho', 'AR': 'Arkansas', 'ME': 'Maine', 'WA': 'Washington', 'HI': 'Hawaii', 'WI': 'Wisconsin', 'MI': 'Michigan', 'IN': 'Indiana', 'NJ': 'New Jersey', 'AZ': 'Arizona', 'GU': 'Guam', 'MS': 'Mississippi', 'PR': 'Puerto Rico', 'NC': 'North Carolina', 'TX': 'Texas', 'SD': 'South Dakota', 'MP': 'Northern Mariana Islands', 'IA': 'Iowa', 'MO': 'Missouri', 'CT': 'Connecticut', 'WV': 'West Virginia', 'SC': 'South Carolina', 'LA': 'Louisiana', 'KS': 'Kansas', 'NY': 'New York', 'NE': 'Nebraska', 'OK': 'Oklahoma', 'FL': 'Florida', 'CA': 'California', 'CO': 'Colorado', 'PA': 'Pennsylvania', 'DE': 'Delaware', 'NM': 'New Mexico', 'RI': 'Rhode Island', 'MN': 'Minnesota', 'VI': 'Virgin Islands', 'NH': 'New Hampshire', 'MA': 'Massachusetts', 'GA': 'Georgia', 'ND': 'North Dakota', 'VA': 'Virginia'}
+
+
+def get_list_of_university_towns():
+    '''return dataframe of university towns'''
+    lst_of_states = []
+    for key, value in states.items():
+        lst_of_states.append(value)
+
+    i = 0
+    with open('university_towns2.txt','r') as file:
+        for line in file:
+            print(line)
+            i += 1
+            if i == 5:
+                break
+
+
+    #state_towns =
+
+    #temp = pd.DataFrame([state_towns], columns=['State','RegionName'])
+
+
+
+print(get_list_of_university_towns())
