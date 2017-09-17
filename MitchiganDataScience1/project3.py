06fcaf09fd9605ae2582a6045c3da293daf8d7e9
Rider One <mathiasbube2@gmail.com>
2017-09-17 12:02:24 -0700

    started project 3

--- MitchiganDataScience1/project3.py
+++ MitchiganDataScience1/project3.py
@@ -19,19 +19,102 @@ def get_list_of_university_towns():
     for key, value in states.items():
         lst_of_states.append(value)
 
-    i = 0
-    with open('university_towns2.txt','r') as file:
+    state_towns = []
+    with open('university_towns.txt','r') as file:
         for line in file:
-            print(line)
-            i += 1
-            if i == 5:
-                break
+            clean = line.replace('[','(').split('(')[0].strip()
+            if clean in lst_of_states:
+                state = clean
+            else:
+                town = clean
+                state_towns.append([state, town])
+
+    return pd.DataFrame(state_towns, columns=['State','RegionName'])
 
 
-    #state_towns =
+def get_recession_start():
+    '''Returns the year and quarter of the recession start time as a
+    string value in a format such as 2005q3'''
+    df = pd.read_excel('gdplev.xls', header=None, skiprows=8, index_col=4)
+    df.drop([0,1,2,3,5,7], axis=1, inplace=True)
+    df.index.name = 'Period'
+    df.rename(columns={6:'GDP (billions)'}, inplace=True)
 
-    #temp = pd.DataFrame([state_towns], columns=['State','RegionName'])
+    narrow_df = df[df.index >= '2000q1']
+    series = narrow_df['GDP (billions)']
+
+    recessions = []
+    for row in range(2, len(series)):
+        if series[row] < series[row - 1] and series[row - 1] < series[row - 2]:
+            recessions.append(series.index[row])
+
+    return recessions[0]
+
+
+def get_recession_end():
+    '''Returns the year and quarter of the recession end time as a
+        string value in a format such as 2005q3'''
+    df = pd.read_excel('gdplev.xls', header=None, skiprows=8, index_col=4)
+    df.drop([0, 1, 2, 3, 5, 7], axis=1, inplace=True)
+    df.index.name = 'Period'
+    df.rename(columns={6: 'GDP (billions)'}, inplace=True)
+
+    narrow_df = df[df.index >= '2000q1']
+    series = narrow_df['GDP (billions)']
+
+    recession = False
+    for row in range(2, len(series)):
+        if series[row] < series[row - 1] and series[row - 1] < series[row - 2]:
+            recession = True
+        if recession == True and series[row] > series[row - 1] and series[row - 1] > series[row - 2]:
+            return series.index[row]
+
+
+def get_recession_bottom():
+    '''Returns the year and quarter of the recession bottom time as a
+    string value in a format such as 2005q3'''
+    df = pd.read_excel('gdplev.xls', header=None, skiprows=8, index_col=4)
+    df.drop([0, 1, 2, 3, 5, 7], axis=1, inplace=True)
+    df.index.name = 'Period'
+    df.rename(columns={6: 'GDP (billions)'}, inplace=True)
+
+    narrow_df = df[df.index >= '2000q1']
+    series = narrow_df['GDP (billions)']
+
+    recession = False
+    recession_bottom_gdp = None
+    recession_bottom = None
+    for row in range(2, len(series)):
+        if series[row] < series[row - 1] and series[row - 1] < series[row - 2]:
+            recession = True
+
+        if recession == True:
+            if recession_bottom_gdp == None or series[row] < recession_bottom_gdp:
+                recession_bottom_gdp = series[row]
+                recession_bottom = series.index[row]
+
+        if recession == True and series[row] > series[row - 1] and series[row - 1] > series[row - 2]:
+            recession = False
+
+    return recession_bottom
+
+
+def convert_housing_data_to_quarters():
+    '''Converts the housing data to quarters and returns it as mean
+       values in a dataframe. This dataframe should be a dataframe with
+       columns for 2000q1 through 2016q3, and should have a multi-index
+       in the shape of ["State","RegionName"].
+
+       Note: Quarters are defined in the assignment description, they are
+       not arbitrary three month periods.
+
+       The resulting dataframe should have 67 columns, and 10,730 rows.
+    '''
+    pass
 
 
 
-print(get_list_of_university_towns())
+
+
+
+print(convert_housing_data_to_quarters())
